8bf3b42b8293a4cc1f3ec35fc682b4503e90f145
“Zelaikha <yosufzaizb@nih.gov>
2025-04-21 17:17:11 -0400

    updated setup file

--- notebooks/FederatedLearning/scripts/setup.py
+++ notebooks/FederatedLearning/scripts/setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 from setuptools import setup, find_packages
 
 setup(
-    name="my_package",
+    name="fl_packages",
     version="0.1",
     packages=find_packages(),
     install_requires=[
